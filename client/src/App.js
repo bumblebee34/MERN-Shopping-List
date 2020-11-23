6ac5a2d0ac5b42817b203758b0bceeafadaae49d
Bhagyaraj Haranale <bhagyarajharanale34@gmail.com>
2020-11-23 10:48:56 +0530

    Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import AppNavbar from './components/AppNavbar';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
     return (
         <div className="App">
-            hiii
+            <AppNavbar />
         </div>
     );
 }
---
+++ client/src/components/AppNavbar.js
@@ -0,0 +1,55 @@
+import React, {Component} from 'react';
+import { 
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem,
+    NavbarText,
+    Container
+} from 'reactstrap';
+import App from '../App';
+
+class AppNavbar extends Component{
+    state = {
+        isOpen: false
+    }
+
+    toggle = () => {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+
+    render() {
+        return(
+            <div>
+            <Navbar color="dark" dark expand="sm" className="mb-5">
+                <Container>
+                    <NavbarBrand href="/">
+                        Shopping List
+                    </NavbarBrand>
+                    <NavbarToggler onClick={this.toggle} />
+                    <Collapse isOpen={this.state.isOpen} navbar>
+                        <Nav className="ml-auto" navbar>
+                            <NavItem>
+                                <NavLink href="https://github.com/bumblebee34">
+                                    Github
+                                </NavLink>
+                            </NavItem>
+                        </Nav>
+                    </Collapse>
+                </Container>
+            </Navbar>
+        </div>
+        )
+    }
+}
+
+export default AppNavbar;
