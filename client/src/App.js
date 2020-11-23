14f226305015ff4faaa544ca5a9544f21307defa
Bhagyaraj Haranale <bhagyarajharanale34@gmail.com>
2020-11-23 09:37:05 +0530

    MERN Front ENd

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            hiii
+        </div>
+    );
+}
+
+export default App;
