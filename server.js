505eac2b5e6c0d6e2fdcca11a1616d52a5a407b2
Bhagyaraj Haranale <bhagyarajharanale34@gmail.com>
2020-11-24 16:17:05 +0530

    Backend

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-
+const path = require('path');
 const items = require('./routes/api/items');
 
 const app = express();
@@ -19,6 +19,14 @@ mongoose
     .catch(() => console.log(err));
 
 app.use('/api/items', items);
+
+// Serve static assets if we are in production
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const port = process.env.PORT || 5000;
 
