e397d1a42b12fdd7d7548e204a6457a1a1cbb924
Bhagyaraj Haranale <bhagyarajharanale34@gmail.com>
2020-11-24 15:43:23 +0530

    Redux Final

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,27 @@
 import React, { Component } from 'react';
 import AppNavbar from './components/AppNavbar';
 import ShoppingList from './components/ShoppingList';
-
+import { Provider } from 'react-redux';
+import ItemModal from './components/ItemModal';
+import { Container } from 'reactstrap';
+import store from './store';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-function App() {
-    return (
-        <div className="App">
-            <AppNavbar />
-            <ShoppingList />
-        </div>
-    );
+class App extends Component {
+    render(){
+        return (
+            <Provider store={store}>
+                 <div className="App">
+                    <AppNavbar />
+                    <Container>
+                        <ItemModal />
+                        <ShoppingList />
+                    </Container>
+                </div>
+            </Provider>
+        );
+    }
 }
 
 export default App;
